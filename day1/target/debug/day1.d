aa1f659028f27e719175c695222f094739a81f22
Stefan Mesken <KappaDistributive@users.noreply.github.com>
2018-12-12 22:25:16 +0100

    updated naming scheme

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/stefan/Documents/Programming/Rust/aoc2017rust/day1/target/debug/day1.dSYM: /Users/stefan/Documents/Programming/Rust/aoc2017rust/day1/src/../input.txt /Users/stefan/Documents/Programming/Rust/aoc2017rust/day1/src/main.rs
--- day1/target/debug/deps/day1-6b1e15078f9e665b.d
+++
@@ -1,6 +0,0 @@
-/Users/stefan/Documents/Programming/Rust/aoc2017rust/day1/target/debug/deps/day1-6b1e15078f9e665b: src/main.rs src/../input.txt
-
-/Users/stefan/Documents/Programming/Rust/aoc2017rust/day1/target/debug/deps/day1-6b1e15078f9e665b.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
